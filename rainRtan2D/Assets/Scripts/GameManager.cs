aec2bef245a74db8e3c2a3471eeba73638ce6f30
devnowk <57873373+devnowk@users.noreply.github.com>
2022-03-14 18:36:19 +0900

    게임 끝내기
    1. 게임 판넬 만들기
      - inactive 상태로 설정
    2. 시간 흐르게 하기
      - GameManager에서 남은시간 설정
    3. 게임 종료
      - 남은 시간이 0일 때, 게임 종료
      - timeScale을 0으로 설정해서 시간 흐르지 않게 함
      - 판넬 띄움
    4. 다시 시작
      - 판넬의 버튼 클릭 시 씬 다시 로드
      - 판넬 버튼 클릭을 위해 판넬 스크립트 필요
      - 초기화 함수 생성 : 씬 다시로드해도 timeScale은 다시 설정해야됨
      - Start()에 초기화 함수 넣어줌 : 씬 로드 시 실행됨

--- rainRtan2D/Assets/Scripts/GameManager.cs
+++ rainRtan2D/Assets/Scripts/GameManager.cs
@@ -1,14 +1,18 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.UI;
+using UnityEngine.UI; // Text 불러오기 위해 필요
+using UnityEngine.SceneManagement; // scene 다시 로드시키기 위해 필요
 
 public class GameManager : MonoBehaviour // 게임 전체를 관장함
 {
     public static GameManager I;
     public GameObject rain; // 빗방울 프리팹
+    public GameObject panel; // 게임 끝 판넬
     public Text scoreText; // 점수판 넣어줘야 함
+    public Text timeText; // 남은시간 텍스트 넣어줘야 함
     int totalScore = 0;
+    float limit = 30; // 남은 시간
 
     void Awake() // 게임 매니저 싱글톤화 시켜줌
     {
@@ -19,12 +23,21 @@ public class GameManager : MonoBehaviour // 게임 전체를 관장함
     void Start()
     {
         InvokeRepeating("makeRain", 0, 0.5f); // 함수를 0초 동안 호출하고 0.5초 마다 이를 반복한다.
+        initGame(); // 씬 다시 로드됐을 때, 초기화 함수 실행
     }
 
     // Update is called once per frame
     void Update()
     {
+        limit -= Time.deltaTime;
+        if(limit < 0)
+        {
+            Time.timeScale = 0.0f; // 시간의 흐름을 지정해줌(1.0이면 원래 속도, 0이면 멈춤)
+            limit = 0.0f;
+            panel.SetActive(true); // 판넬 액티브 상태로 바꿔줌
+        }
 
+        timeText.text = limit.ToString("N0"); // N2 : 소수점 둘째 자리까지 잘라서 문자열로 바꿔줌   
     }
 
     void makeRain()
@@ -37,5 +50,17 @@ public class GameManager : MonoBehaviour // 게임 전체를 관장함
     {
         totalScore += score; // 빗방울을 먹으면 점수가 올라감
         scoreText.text = totalScore.ToString(); // text에는 문자열이 들어가야 함
+    }
+
+    public void retry()
+    {
+        SceneManager.LoadScene("MainScene"); // MainScene을 다시 로드함(처음 설정한 그대로)
+    }
+
+    void initGame() // 씬이 다시 로드 됐을 때 초기화 해줄 변수들, Start()에 넣으면 씬 로드 시 실행
+    {
+        Time.timeScale = 1.0f; // 원래대로 시간 흐르도록 설정
+        totalScore = 0;
+        limit = 30.0f;
     }
 }
