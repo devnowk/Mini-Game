bea2f27017b610625ff6201de848077509f5919b
devnowk <57873373+devnowk@users.noreply.github.com>
2022-06-21 01:36:09 +0900

    시간 흐르게 하기
    - gameManager 추가해서 Time.deltaTime으로 시간 흐르게 하기
    - timeTxt에 적용

---
+++ findRtan2D/Assets/Scriptes/gameManager.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class gameManager : MonoBehaviour
+{
+    public Text timeTxt;
+    float time;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        time += Time.deltaTime;
+        timeTxt.text = time.ToString("N2"); // 소수 둘째자리까지
+    }
+}
