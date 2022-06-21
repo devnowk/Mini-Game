321c369e7dc0d7813fdba7ee47102ebf950812ac
devnowk <57873373+devnowk@users.noreply.github.com>
2022-06-21 22:26:19 +0900

    카드 배치하기
    - gameManager에서 카드 배치하기
    - card를 cards 밑에 생성(parent)
    - card의 position 규칙 찾은 후 배치하기
    - card들의 전체 위치 조정

--- findRtan2D/Assets/Scriptes/gameManager.cs
+++ findRtan2D/Assets/Scriptes/gameManager.cs
@@ -6,12 +6,24 @@ using UnityEngine.UI;
 public class gameManager : MonoBehaviour
 {
     public Text timeTxt;
+    public GameObject card;
     float time;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        for(int i=0; i<16; i++)
+        {
+            GameObject newCard = Instantiate(card); // 인스턴스 객체 저장
+            // newCard의 위치를 cards밑으로 옮기기
+            newCard.transform.parent = GameObject.Find("cards").transform;
+            // i = (0,1,2,3) (4,5,6,7) (8,9,10,11) (12,13,14,15)
+            // x = (0,1,2,3) (0,1,2,3) (0,1,2,3) (0,1,2,3) * 1.4 (나머지 계산)
+            // y = (0,0,0,0) (1,1,1,1) (2,2,2,2) (3,3,3,3) * -1.4 (몫 계산)
+            float x = (i % 4) * 1.4f - 2.1f;
+            float y = (i / 4) * -1.4f + 0.3f;
+            newCard.transform.position = new Vector3(x, y, 0);
+        }
     }
 
     // Update is called once per frame
